f2117039fcb95f95070d1fc6290721189f149074
kjk71 <kks007159@naver.com>
2024-05-15 14:02:20 +0900

    Rect

---
+++ src/RectEscape/RectEscape.java
@@ -0,0 +1,25 @@
+package RectEscape;
+
+import java.util.*;
+
+public class RectEscape {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		int num2 = sc.nextInt();
+		int num3 = sc.nextInt();
+		int num4  = sc.nextInt();
+		
+		ArrayList<Integer> list  = new ArrayList<Integer>();
+		
+		list.add(num);
+		list.add(num2);
+		list.add(num3-num);
+		list.add(num4-num2);
+		
+		int min = Collections.min(list);
+		System.out.print(min);
+	}
+}
